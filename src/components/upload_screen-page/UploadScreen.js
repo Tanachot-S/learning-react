0a0d09c3e80f3ffc8c9931bc947b4e98928e4f4d
Tanachot <ballzakub555@gmail.com>
2018-02-22 11:05:33 +0700

    init project

---
+++ src/components/upload_screen-page/UploadScreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import axios from 'axios';
+
+class Login extends Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return ({});
+    }
+}
